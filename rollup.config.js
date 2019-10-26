cf78469148a348d1b2e02a8911cb3f2a66cc4318
Wasuwat Limsuparhat <wasuwat.limsuparhat@thomsonreuters.com>
2019-10-26 18:25:13 +0700

    WIP

---
+++ rollup.config.js
@@ -0,0 +1,30 @@
+import resolve from 'rollup-plugin-node-resolve';
+import babel from 'rollup-plugin-babel';
+
+export default {
+    input: ['src/index.js'],
+    output: {
+        file: 'build/index.js',
+        format: 'es',
+    },
+    plugins: [
+        resolve(),
+        babel({
+            presets: [
+                [
+                    '@babel/preset-env',
+                    {
+                        modules: 'false',
+                        targets: {
+                            browsers: '> 1%, IE 11, not dead',
+                        },
+                    }
+                ]
+            ],
+            plugins: [
+                '@babel/plugin-proposal-class-properties',
+                ['@babel/proposal-decorators', { decoratorsBeforeExport: true }],
+            ],
+        })
+    ]
+};
---
+++ src/awesome-panel.js
@@ -0,0 +1,31 @@
+import { LitElement, html, css } from 'lit-element';
+
+class AwesomePanel extends LitElement {
+    static get styles() {
+        return css`
+          div { 
+                max-width: 100%;
+                max-height: 100%;
+                padding: 1.5rem;
+                background-color: #fff;
+                background: linear-gradient(#f8f8f8, #fff);
+                box-shadow: 0 8px 16px -8px rgba(0,0,0,0.4);
+                border-radius: 6px;
+                overflow: hidden;
+                position: relative;
+                margin: 1.5rem;  
+            }
+        `;
+    }
+
+    render() {
+        return html`
+            <div>
+                <slot></slot>
+            </div>
+        `;
+    }
+}
+
+
+customElements.define('awesome-panel', AwesomePanel);
