cbf20da74cef441e6d20a113f7c05f29803e4d5e
Wasuwat Limsuparhat <wasuwat.se@gmail.com>
2019-10-27 09:27:40 +0700

    add modern and legacy bundle setup

--- rollup.config.js
+++ rollup.config.js
@@ -1,26 +1,42 @@
 import resolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
+import { terser } from 'rollup-plugin-terser';
 
-export default {
-    input: ['src/index.js'],
-    output: {
-        file: 'build/index.js',
-        format: 'esm',
+export default [
+    {
+        input: ['src/index.js'],
+        output: {
+            file: 'build/index.js',
+            format: 'esm',
+        },
+        plugins: [
+            resolve(),
+            terser()
+        ]
     },
-    plugins: [
-        resolve(),
-        babel({
-            presets: [
-                [
-                    '@babel/preset-env',
-                    {
-                        modules: 'false',
-                        targets: {
-                            browsers: '> 1%, IE 11, not dead',
-                        },
-                    }
+    {
+        input: ['src/index.js'],
+        output: {
+            file: 'build/index.legacy.js',
+            format: 'esm',
+        },
+        plugins: [
+            resolve(),
+            babel({
+                presets: [
+                    [
+                        '@babel/preset-env',
+                        {
+                            useBuiltIns: false,
+                            modules: false,
+                            targets: {
+                                browsers: '> 1%, IE 11, not dead',
+                            },
+                        }
+                    ]
                 ]
-            ]
-        })
-    ]
-};
+            }),
+            terser()
+        ]
+    }
+];
