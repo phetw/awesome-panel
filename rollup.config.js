be6ed3ed64303a4d1448619d71bbd11e471710b1
phet.w <wasuwat.se@gmail.com>
2020-10-18 18:06:59 +0700

    feat: delegate transpile to app

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,4 @@
 import resolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
-import babel from 'rollup-plugin-babel';
 import filesize from 'rollup-plugin-filesize';
 import { terser } from 'rollup-plugin-terser';
 
@@ -12,35 +10,5 @@ export default [
       format: 'esm',
     },
     plugins: [resolve(), terser(), filesize()],
-  },
-  {
-    input: ['src/awesome-panel.js'],
-    output: {
-      file: 'build/awesome-panel.es5.js',
-      format: 'esm',
-    },
-    plugins: [
-      resolve(),
-      commonjs({
-        include: 'node_modules/**',
-      }),
-      babel({
-        runtimeHelpers: true,
-        presets: [
-          [
-            '@babel/preset-env',
-            {
-              modules: false,
-              targets: {
-                browsers: '> 1%, IE 11, not dead',
-              },
-            },
-          ],
-        ],
-        plugins: ['@babel/plugin-transform-runtime'],
-      }),
-      terser(),
-      filesize(),
-    ],
   },
 ];
