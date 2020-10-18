5979f69fa5451bbbd1207fb54c574a9d837d9474
phet.w <wasuwat.se@gmail.com>
2020-10-18 17:05:57 +0700

    feat: update toolings and add testings

--- src/awesome-panel.js
+++ src/awesome-panel.js
@@ -1,30 +1,34 @@
 import { LitElement, html, css } from 'lit-element';
 
 class AwesomePanel extends LitElement {
-    static get styles() {
-        return css`
-          div { 
-                max-width: 100%;
-                max-height: 100%;
-                padding: 1.5rem;
-                background-color: #fff;
-                background: linear-gradient(#f8f8f8, #fff);
-                box-shadow: 0 8px 16px -8px rgba(0,0,0,0.4);
-                border-radius: 6px;
-                overflow: hidden;
-                position: relative;
-                margin: 1.5rem;  
-            }
-        `;
-    }
+  static get styles() {
+    return css`
+      :host {
+        display: block;
+        padding: 1.5rem;
+        background-color: #fff;
+        box-shadow: 0 8px 16px -8px rgba(0, 0, 0, 0.4);
+        border-radius: 6px;
+        margin: 1.5rem;
+      }
+      h1 {
+        margin: 0 0 1rem 0;
+      }
+    `;
+  }
 
-    render() {
-        return html`
-            <div>
-                <slot></slot>
-            </div>
-        `;
-    }
+  static get properties() {
+    return {
+      header: { type: String },
+    };
+  }
+
+  render() {
+    return html`
+      <h1>${this.header}</h1>
+      <slot></slot>
+    `;
+  }
 }
 
 customElements.define('awesome-panel', AwesomePanel);
---
+++ test/awesome-panel.test.js
@@ -0,0 +1,13 @@
+import { html, fixture, expect } from '@open-wc/testing';
+import '../src/awesome-panel.js';
+
+describe('awesome-panel', () => {
+  it('can semantically compare light DOM trees', async () => {
+    const el = await fixture(html`<awesome-panel><h1>Light DOM</h1></awesome-panel>`);
+    expect(el).lightDom.to.equal('<h1>Light DOM</h1>');
+  });
+  it('can access shadow DOM', async () => {
+    const el = await fixture(html`<awesome-panel>Shadow DOM</awesome-panel>`);
+    await expect(el).shadowDom.to.be.accessible();
+  });
+});
